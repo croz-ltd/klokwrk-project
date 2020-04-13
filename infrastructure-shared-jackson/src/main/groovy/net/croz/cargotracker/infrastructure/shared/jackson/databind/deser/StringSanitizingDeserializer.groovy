6f42057119a77251eb81001a9f03252a6a148b3c
Damir Murat <damir.murat.git@gmail.com>
2020-04-13 16:19:31 +0200

    Adding CompileStatic on infrastructure-shared-jackson modules.

--- infrastructure-shared-jackson/src/main/groovy/net/croz/cargotracker/infrastructure/shared/jackson/databind/deser/StringSanitizingDeserializer.groovy
+++ infrastructure-shared-jackson/src/main/groovy/net/croz/cargotracker/infrastructure/shared/jackson/databind/deser/StringSanitizingDeserializer.groovy
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import groovy.transform.CompileStatic
 
 /**
  * Jackson deserializer which deserializes any empty string as <code>null</code>.
  */
+@CompileStatic
 class StringSanitizingDeserializer extends StdDeserializer<String> {
   StringSanitizingDeserializer() {
     super(String)
@@ -15,7 +17,7 @@ class StringSanitizingDeserializer extends StdDeserializer<String> {
 
   @Override
   String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) {
-    JsonNode node = jsonParser.readValueAsTree()
+    JsonNode node = jsonParser.readValueAsTree() as JsonNode
     if (node.asText().trim().isEmpty()) {
       return null
     }
