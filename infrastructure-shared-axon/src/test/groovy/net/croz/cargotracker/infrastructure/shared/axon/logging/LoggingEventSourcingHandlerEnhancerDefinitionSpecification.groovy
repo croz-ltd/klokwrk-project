f9035158cccda6c9c06b7677a9bbc68010fdc8a0
Damir Murat <damir.murat.git@gmail.com>
2020-04-16 19:24:45 +0200

    Adding LoggingEventSourcingHandlerEnhancerDefinition.

---
+++ infrastructure-shared-axon/src/test/groovy/net/croz/cargotracker/infrastructure/shared/axon/logging/LoggingEventSourcingHandlerEnhancerDefinitionSpecification.groovy
@@ -0,0 +1,114 @@
+package net.croz.cargotracker.infrastructure.shared.axon.logging
+
+import com.google.common.collect.ImmutableList
+import net.croz.cargotracker.infrastructure.shared.axon.logging.stub.aggregate.MyTestAggregate
+import net.croz.cargotracker.infrastructure.shared.axon.logging.stub.command.CreateMyTestAggregateCommand
+import net.croz.cargotracker.infrastructure.shared.axon.logging.stub.command.UpdateMyTestAggregateCommand
+import org.axonframework.commandhandling.gateway.CommandGateway
+import org.axonframework.config.Configuration
+import org.axonframework.config.Configurer
+import org.axonframework.config.DefaultConfigurer
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine
+import org.axonframework.messaging.annotation.ClasspathHandlerDefinition
+import org.axonframework.messaging.annotation.ClasspathHandlerEnhancerDefinition
+import org.axonframework.messaging.annotation.MultiHandlerDefinition
+import org.axonframework.messaging.annotation.MultiHandlerEnhancerDefinition
+import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
+import uk.org.lidalia.slf4jext.Level
+import uk.org.lidalia.slf4jtest.LoggingEvent
+import uk.org.lidalia.slf4jtest.TestLogger
+import uk.org.lidalia.slf4jtest.TestLoggerFactory
+
+class LoggingEventSourcingHandlerEnhancerDefinitionSpecification extends Specification {
+  Configuration axonConfiguration
+  CommandGateway axonCommandGateway
+
+  def setup() {
+    TestLoggerFactory.clearAll()
+//    TestLoggerFactory.getInstance().setPrintLevel(Level.DEBUG) // uncomment if you want to see logging output during the test
+
+    Configurer axonConfigurer = DefaultConfigurer.defaultConfiguration()
+    axonConfigurer.configureEmbeddedEventStore((Configuration axonConfiguration) -> new InMemoryEventStorageEngine())
+                  .configureAggregate(MyTestAggregate)
+                  .registerHandlerDefinition((Configuration configuration, Class inspectedClass) -> {
+                    MultiHandlerDefinition multiHandlerDefinition = MultiHandlerDefinition.ordered(
+                        MultiHandlerEnhancerDefinition.ordered(ClasspathHandlerEnhancerDefinition.forClass(inspectedClass), new LoggingEventSourcingHandlerEnhancerDefinition()),
+                        ClasspathHandlerDefinition.forClass(inspectedClass)
+                    )
+
+                    return multiHandlerDefinition
+                  })
+
+    axonConfiguration = axonConfigurer.buildConfiguration()
+    axonConfiguration.start()
+
+    axonCommandGateway = axonConfiguration.commandGateway()
+  }
+
+  @SuppressWarnings("unused")
+  def cleanup() {
+    TestLoggerFactory.clearAll()
+
+    axonConfiguration.shutdown()
+    axonCommandGateway = null
+    axonConfiguration = null
+  }
+
+  def "should work for event sourcing handler"() {
+    given:
+    TestLogger logger = TestLoggerFactory.getTestLogger("cargotracker.axon.eventsourcing-handler-logging")
+    String aggregateIdentifier = UUID.randomUUID().toString()
+
+    when:
+    axonCommandGateway.sendAndWait(new CreateMyTestAggregateCommand(aggregateIdentifier: aggregateIdentifier, name: "bla"))
+    axonCommandGateway.sendAndWait(new UpdateMyTestAggregateCommand(aggregateIdentifier: aggregateIdentifier, sequenceNumber: 0, name: "ble"))
+    axonCommandGateway.sendAndWait(new UpdateMyTestAggregateCommand(aggregateIdentifier: aggregateIdentifier, sequenceNumber: 1, name: "bla ble"))
+
+    then:
+    new PollingConditions(timeout: 5, initialDelay: 0.5, delay: 0.5).eventually {
+      ImmutableList<LoggingEvent> loggingEvents = logger.getAllLoggingEvents()
+      loggingEvents.size() == 6
+      loggingEvents*.level == (0..5).collect({ Level.DEBUG })
+
+      // sample message: Executing EventSourcingHandler method [MyTestAggregate.onMyTestAggregateCreatedEvent(MyTestAggregateCreatedEvent)] with event [eventId: 38a0c0fa-0b79-456a-b9cc-a285f97e1e09, MyTestAggregateCreatedEvent(aggregateIdentifier: ae587429-a07b-4e4c-a251-25691a38944e, sequenceNumber: 0)]
+      3 == loggingEvents.findAll({
+        Boolean containsAggregateIdentifier = it.message.contains(aggregateIdentifier)
+        Boolean matchesRegex = it.message ==~ /Executing EventSourcingHandler method \[MyTestAggregate.onMyTestAggregateCreatedEvent\(MyTestAggregateCreatedEvent\)] with event \[eventId: \p{Graph}{36}, MyTestAggregateCreatedEvent\(aggregateIdentifier: \p{Graph}{36}, sequenceNumber: 0\)]/
+        return containsAggregateIdentifier && matchesRegex
+      }).size()
+
+      // sample message: Executing EventSourcingHandler method [MyTestAggregate.onMyTestAggregateUpdatedEvent(MyTestAggregateUpdatedEvent)] with event [eventId: 91fb0e76-1e85-488a-84a4-d51bfdc548c7, MyTestAggregateUpdatedEvent(aggregateIdentifier: ae587429-a07b-4e4c-a251-25691a38944e, sequenceNumber: 1)]
+      2 == loggingEvents.findAll({
+        Boolean containsAggregateIdentifier = it.message.contains(aggregateIdentifier)
+        Boolean matchesRegex = it.message ==~ /Executing EventSourcingHandler method \[MyTestAggregate.onMyTestAggregateUpdatedEvent\(MyTestAggregateUpdatedEvent\)] with event \[eventId: \p{Graph}{36}, MyTestAggregateUpdatedEvent\(aggregateIdentifier: \p{Graph}{36}, sequenceNumber: 1\)]/
+        return containsAggregateIdentifier && matchesRegex
+      }).size()
+
+      // sample message: Executing EventSourcingHandler method [MyTestAggregate.onMyTestAggregateUpdatedEvent(MyTestAggregateUpdatedEvent)] with event [eventId: e034db67-8d7c-496e-be6f-9ad7b07f271a, MyTestAggregateUpdatedEvent(aggregateIdentifier: ae587429-a07b-4e4c-a251-25691a38944e, sequenceNumber: 2)]
+      1 == loggingEvents.findAll({
+        Boolean containsAggregateIdentifier = it.message.contains(aggregateIdentifier)
+        Boolean matchesRegex = it.message ==~ /Executing EventSourcingHandler method \[MyTestAggregate.onMyTestAggregateUpdatedEvent\(MyTestAggregateUpdatedEvent\)] with event \[eventId: \p{Graph}{36}, MyTestAggregateUpdatedEvent\(aggregateIdentifier: \p{Graph}{36}, sequenceNumber: 2\)]/
+        return containsAggregateIdentifier && matchesRegex
+      }).size()
+    }
+  }
+
+  def "should not log for logger level higher than DEBUG"() {
+    given:
+    TestLogger logger = TestLoggerFactory.getTestLogger("cargotracker.axon.eventsourcing-handler-logging")
+    logger.setEnabledLevelsForAllThreads(Level.INFO)
+    String aggregateIdentifier = UUID.randomUUID().toString()
+
+    when:
+    axonCommandGateway.sendAndWait(new CreateMyTestAggregateCommand(aggregateIdentifier: aggregateIdentifier, name: "bla"))
+    axonCommandGateway.sendAndWait(new UpdateMyTestAggregateCommand(aggregateIdentifier: aggregateIdentifier, sequenceNumber: 0, name: "ble"))
+    axonCommandGateway.sendAndWait(new UpdateMyTestAggregateCommand(aggregateIdentifier: aggregateIdentifier, sequenceNumber: 1, name: "bla ble"))
+
+    then:
+    new PollingConditions(timeout: 5, initialDelay: 0.5, delay: 0.5).eventually {
+      ImmutableList<LoggingEvent> loggingEvents = logger.getAllLoggingEvents()
+      loggingEvents.size() == 0
+    }
+  }
+}
