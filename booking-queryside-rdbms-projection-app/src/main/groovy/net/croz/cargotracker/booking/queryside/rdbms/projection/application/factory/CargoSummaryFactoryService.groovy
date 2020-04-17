869aed295b3af551870822b263fbabba99aa672b
Damir Murat <damir.murat.git@gmail.com>
2020-04-17 12:35:30 +0200

    Populating aggregateSequenceNumber via Axon's DomainEventMessage.

--- booking-queryside-rdbms-projection-app/src/main/groovy/net/croz/cargotracker/booking/queryside/rdbms/projection/application/factory/CargoSummaryFactoryService.groovy
+++ booking-queryside-rdbms-projection-app/src/main/groovy/net/croz/cargotracker/booking/queryside/rdbms/projection/application/factory/CargoSummaryFactoryService.groovy
@@ -3,15 +3,16 @@ package net.croz.cargotracker.booking.queryside.rdbms.projection.application.fac
 import groovy.transform.CompileStatic
 import net.croz.cargotracker.booking.api.axon.event.CargoBookedEvent
 import net.croz.cargotracker.booking.queryside.rdbms.domain.querymodel.CargoSummaryQueryEntity
+import org.axonframework.eventhandling.DomainEventMessage
 
 @CompileStatic
 class CargoSummaryFactoryService {
-  static CargoSummaryQueryEntity createCargoSummaryQueryEntity(CargoBookedEvent cargoBookedEvent) {
+  static CargoSummaryQueryEntity createCargoSummaryQueryEntity(CargoBookedEvent cargoBookedEvent, DomainEventMessage domainEventMessage) {
     // TODO dmurat: automate populating persistent entity
     String aggregateIdentifier = cargoBookedEvent.aggregateIdentifier
     String originLocation = cargoBookedEvent.originLocation.unLoCode.code
     String destinationLocation = cargoBookedEvent.destinationLocation.unLoCode.code
-    Long aggregateSequenceNumber = 0
+    Long aggregateSequenceNumber = domainEventMessage.sequenceNumber
 
     CargoSummaryQueryEntity cargoSummaryQueryEntity = new CargoSummaryQueryEntity(
         aggregateIdentifier: aggregateIdentifier, aggregateSequenceNumber: aggregateSequenceNumber, originLocation: originLocation, destinationLocation: destinationLocation
