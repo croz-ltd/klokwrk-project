c89871d23167522f4015460eb2336516071cda71
Damir Murat <damir.murat.git@gmail.com>
2020-04-20 23:03:54 +0200

    Adding locale into OperationRequest's metaData from inbound rest channel.

--- booking-queryside-app/src/main/groovy/net/croz/cargotracker/booking/queryside/interfaces/web/controller/CargoBookingQueryController.groovy
+++ booking-queryside-app/src/main/groovy/net/croz/cargotracker/booking/queryside/interfaces/web/controller/CargoBookingQueryController.groovy
@@ -1,12 +1,14 @@
 package net.croz.cargotracker.booking.queryside.interfaces.web.controller
 
 import groovy.transform.CompileStatic
+import net.croz.cargotracker.api.open.shared.conversation.MetaDataConstant
 import net.croz.cargotracker.api.open.shared.conversation.OperationRequest
 import net.croz.cargotracker.api.open.shared.conversation.OperationResponse
 import net.croz.cargotracker.booking.api.open.queryside.conversation.CargoSummaryQueryRequest
 import net.croz.cargotracker.booking.api.open.queryside.conversation.CargoSummaryQueryResponse
 import net.croz.cargotracker.booking.queryside.application.CargoBookingQueryApplicationService
 import net.croz.cargotracker.booking.queryside.interfaces.web.conversation.CargoSummaryQueryWebRequest
+import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -29,6 +31,11 @@ class CargoBookingQueryController {
   }
 
   static OperationRequest<CargoSummaryQueryRequest> cargoSummaryQueryWebRequestToCargoSummaryQueryOperationRequest(CargoSummaryQueryWebRequest cargoSummaryWebRequest) {
-    return new OperationRequest<CargoSummaryQueryRequest>(payload: new CargoSummaryQueryRequest(cargoSummaryWebRequest.properties))
+    OperationRequest<CargoSummaryQueryRequest> operationRequest = new OperationRequest<CargoSummaryQueryRequest>(
+        payload: new CargoSummaryQueryRequest(cargoSummaryWebRequest.properties),
+        metaData: [(MetaDataConstant.INBOUND_CHANNEL_REQUEST_LOCALE_KEY): LocaleContextHolder.getLocale()]
+    )
+
+    return operationRequest
   }
 }
