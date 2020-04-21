1179e77a2a35156ecea675572ea5f7ae569ed08d
Damir Murat <damir.murat.git@gmail.com>
2020-04-21 21:55:43 +0200

    Adding missing CompileStatic.

--- booking-queryside-app/src/main/groovy/net/croz/cargotracker/booking/queryside/infrastructure/spring/web/mvc/RestResponseBodyControllerAdvice.groovy
+++ booking-queryside-app/src/main/groovy/net/croz/cargotracker/booking/queryside/infrastructure/spring/web/mvc/RestResponseBodyControllerAdvice.groovy
@@ -1,8 +1,10 @@
 package net.croz.cargotracker.booking.queryside.infrastructure.spring.web.mvc
 
+import groovy.transform.CompileStatic
 import net.croz.cargotracker.infrastructure.web.spring.mvc.RestResponseBodyAdvice
 import org.springframework.web.bind.annotation.ControllerAdvice
 
 @ControllerAdvice
+@CompileStatic
 class RestResponseBodyControllerAdvice extends RestResponseBodyAdvice {
 }
