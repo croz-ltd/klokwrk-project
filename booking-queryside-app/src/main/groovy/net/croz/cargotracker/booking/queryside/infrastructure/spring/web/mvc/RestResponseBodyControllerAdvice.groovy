15030b694adef63c03797636fd5a19f15ce22707
Damir Murat <damir.murat.git@gmail.com>
2020-04-21 21:46:08 +0200

    Adding RestResponseBodyControllerAdvice into th spring boot config of booking-queryside-app.

---
+++ booking-queryside-app/src/main/groovy/net/croz/cargotracker/booking/queryside/infrastructure/spring/web/mvc/RestResponseBodyControllerAdvice.groovy
@@ -0,0 +1,8 @@
+package net.croz.cargotracker.booking.queryside.infrastructure.spring.web.mvc
+
+import net.croz.cargotracker.infrastructure.web.spring.mvc.RestResponseBodyAdvice
+import org.springframework.web.bind.annotation.ControllerAdvice
+
+@ControllerAdvice
+class RestResponseBodyControllerAdvice extends RestResponseBodyAdvice {
+}
