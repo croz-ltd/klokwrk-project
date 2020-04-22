8dbf8d9974b8bae4cd10c33806c3257b4344ceae
Damir Murat <damir.murat.git@gmail.com>
2020-04-22 12:21:48 +0200

    Updating booking-commandside-app module's spring boot configuration with controller advices.

---
+++ booking-commandside-app/src/main/groovy/net/croz/cargotracker/booking/commandside/infrastructure/spring/web/mvc/RestResponseBodyControllerAdvice.groovy
@@ -0,0 +1,10 @@
+package net.croz.cargotracker.booking.commandside.infrastructure.spring.web.mvc
+
+import groovy.transform.CompileStatic
+import net.croz.cargotracker.infrastructure.web.spring.mvc.RestResponseBodyAdvice
+import org.springframework.web.bind.annotation.ControllerAdvice
+
+@ControllerAdvice
+@CompileStatic
+class RestResponseBodyControllerAdvice extends RestResponseBodyAdvice {
+}
