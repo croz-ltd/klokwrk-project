ea8cbc1ff0f776aa2aa0212de4d22a6b5ba995ae
Damir Murat <damir.murat.git@gmail.com>
2020-04-24 14:30:21 +0200

    Introducing Location.create() factory method.

--- booking-commandside-app/src/main/groovy/net/croz/cargotracker/booking/commandside/infrastructure/repository/InMemoryLocationRegistryRepositoryService.groovy
+++ booking-commandside-app/src/main/groovy/net/croz/cargotracker/booking/commandside/infrastructure/repository/InMemoryLocationRegistryRepositoryService.groovy
@@ -2,10 +2,7 @@ package net.croz.cargotracker.booking.commandside.infrastructure.repository
 
 import groovy.transform.CompileStatic
 import net.croz.cargotracker.booking.commandside.application.repository.LocationRegistryRepositoryService
-import net.croz.cargotracker.booking.domain.model.InternationalizedName
 import net.croz.cargotracker.booking.domain.model.Location
-import net.croz.cargotracker.booking.domain.model.UnLoCode
-import net.croz.cargotracker.booking.domain.model.UnLoCodeFunction
 import org.springframework.stereotype.Service
 
 // TODO dmurat: implement real registry
@@ -13,69 +10,23 @@ import org.springframework.stereotype.Service
 @CompileStatic
 class InMemoryLocationRegistryRepositoryService implements LocationRegistryRepositoryService {
   Map<String, Location> locationMockRegistry = [
-      "HRALJ": new Location(
-          unLoCode: new UnLoCode(code: "HRALJ"), name: new InternationalizedName(name: "Aljmaš"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1-3-----")
-      ),
-      "HRBAK": new Location(
-          unLoCode: new UnLoCode(code: "HRBAK"), name: new InternationalizedName(name: "Bakar"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "123-----")
-      ),
-      "HRBAS": new Location(
-          unLoCode: new UnLoCode(code: "HRBAS"), name: new InternationalizedName(name: "Baška"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1-3-----")
-      ),
-      "HRDBV": new Location(
-          unLoCode: new UnLoCode(code: "HRDBV"), name: new InternationalizedName(name: "Dubrovnik"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1-34----")
-      ),
-      "HRKOR": new Location(
-          unLoCode: new UnLoCode(code: "HRKOR"), name: new InternationalizedName(name: "Korčula"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1-3-----")
-      ),
-      "HRKRK": new Location(
-          unLoCode: new UnLoCode(code: "HRKRK"), name: new InternationalizedName(name: "Krk"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1-3-----")
-      ),
-      "HROSI": new Location(
-          unLoCode: new UnLoCode(code: "HROSI"), name: new InternationalizedName(name: "Osijek"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-      ),
-      "HRPUY": new Location(
-          unLoCode: new UnLoCode(code: "HRPUY"), name: new InternationalizedName(name: "Pula"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-      ),
-      "HRRJK": new Location(
-          unLoCode: new UnLoCode(code: "HRRJK"), name: new InternationalizedName(name: "Rijeka"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-      ),
-      "HRSPU": new Location(
-          unLoCode: new UnLoCode(code: "HRSPU"), name: new InternationalizedName(name: "Split"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-      ),
-      "HRVUK": new Location(
-          unLoCode: new UnLoCode(code: "HRVUK"), name: new InternationalizedName(name: "Vukovar"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "123-5--B")
-      ),
-      "HRZAD": new Location(
-          unLoCode: new UnLoCode(code: "HRZAD"), name: new InternationalizedName(name: "Zadar"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-      ),
-      "HRZAG": new Location(
-          unLoCode: new UnLoCode(code: "HRZAG"), name: new InternationalizedName(name: "Zagreb"), countryName: new InternationalizedName(name: "Hrvatska"),
-          unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "-2345---")
-      )
+      "HRALJ": Location.create("HRALJ", "Aljmaš", "Hrvatska", "1-3-----"),
+      "HRBAK": Location.create("HRBAK", "Bakar", "Hrvatska", "123-----"),
+      "HRBAS": Location.create("HRBAS", "Baška", "Hrvatska", "1-3-----"),
+      "HRDBV": Location.create("HRDBV", "Dubrovnik", "Hrvatska", "1-34----"),
+      "HRKOR": Location.create("HRKOR", "Korčula", "Hrvatska", "1-3-----"),
+      "HRKRK": Location.create("HRKRK", "Krk", "Hrvatska", "1-3-----"),
+      "HROSI": Location.create("HROSI", "Osijek", "Hrvatska", "1234----"),
+      "HRPUY": Location.create("HRPUY", "Pula", "Hrvatska", "1234----"),
+      "HRRJK": Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----"),
+      "HRSPU": Location.create("HRSPU", "Split", "Hrvatska", "1234----"),
+      "HRVUK": Location.create("HRVUK", "Vukovar", "Hrvatska", "123-5--B"),
+      "HRZAD": Location.create("HRZAD", "Zadar", "Hrvatska", "1234----"),
+      "HRZAG": Location.create("HRZAG", "Zagreb", "Hrvatska", "-2345---")
   ]
 
   Location findByUnLoCode(String unLoCode) {
-    Location locationFound = locationMockRegistry.get(
-        unLoCode,
-        new Location(
-            unLoCode: UnLoCode.UNKNOWN_UN_LO_CODE, name: InternationalizedName.UNKNOWN_INTERNATIONALIZED_NAME, countryName: InternationalizedName.UNKNOWN_INTERNATIONALIZED_NAME,
-            unLoCodeFunction: UnLoCodeFunction.UNKNOWN_UN_LO_CODE_FUNCTION
-        )
-    )
-
+    Location locationFound = locationMockRegistry.get(unLoCode, Location.UNKNOWN_LOCATION)
     return locationFound
   }
 }
--- booking-domain-model/src/test/groovy/net/croz/cargotracker/booking/domain/model/LocationSpecification.groovy
+++ booking-domain-model/src/test/groovy/net/croz/cargotracker/booking/domain/model/LocationSpecification.groovy
@@ -21,7 +21,36 @@ class LocationSpecification extends Specification {
   @Unroll
   def "map constructor should fail for invalid input params: [code: #codeParameter, name: #nameParameter, countryName: #countryNameParameter, function: #functionParameter]"() {
     when:
-    new Location(code: new UnLoCode(code: codeParameter), name: new InternationalizedName(name: nameParameter), countryName: new InternationalizedName(name: countryNameParameter))
+    new Location(
+        code: new UnLoCode(code: codeParameter), name: new InternationalizedName(name: nameParameter), countryName: new InternationalizedName(name: countryNameParameter),
+        unLoCodeFunction: new UnLoCodeFunction(functionParameter)
+    )
+
+    then:
+    thrown(IllegalArgumentException)
+
+    where:
+    codeParameter | nameParameter | countryNameParameter | functionParameter
+    null          | "someName"    | "someCountry"        | "0------"
+    "HRRJK"       | null          | "someCountry"        | "0------"
+    "HRRJK"       | "someName"    | null                 | "0------"
+    "HRRJK"       | "someName"    | "someCountry"        | null
+  }
+
+  def "create() factory method should work for correct input params"() {
+    when:
+    Location location = Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----")
+
+    then:
+    location.unLoCode == new UnLoCode(code: "HRRJK")
+    location.name == new InternationalizedName(name: "Rijeka")
+    location.countryName == new InternationalizedName(name: "Hrvatska")
+  }
+
+  @Unroll
+  def "create() factory method should fail for invalid input params: [code: #codeParameter, name: #nameParameter, countryName: #countryNameParameter, function: #functionParameter]"() {
+    when:
+    Location.create(codeParameter, nameParameter, countryNameParameter, functionParameter)
 
     then:
     thrown(IllegalArgumentException)
@@ -37,15 +66,8 @@ class LocationSpecification extends Specification {
   @SuppressWarnings("GroovyPointlessBoolean")
   def "canAcceptCargoFrom() should return false for same locations"() {
     given:
-    Location originLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRRJK"), name: new InternationalizedName(name: "Rijeka"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-    )
-
-    Location destinationLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRRJK"), name: new InternationalizedName(name: "Rijeka"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-    )
+    Location originLocation = Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----")
+    Location destinationLocation = Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----")
 
     when:
     Boolean canAccept = destinationLocation.canAcceptCargoFrom(originLocation)
@@ -57,10 +79,7 @@ class LocationSpecification extends Specification {
   @SuppressWarnings("GroovyPointlessBoolean")
   def "canAcceptCargoFrom() should return false when origin location is null"() {
     given:
-    Location destinationLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRZAD"), name: new InternationalizedName(name: "Zadar"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-    )
+    Location destinationLocation = Location.create("HRZAD", "Zadar", "Hrvatska", "1234----")
 
     when:
     Boolean canAccept = destinationLocation.canAcceptCargoFrom(null)
@@ -72,15 +91,8 @@ class LocationSpecification extends Specification {
   @SuppressWarnings("GroovyPointlessBoolean")
   def "canAcceptCargoFrom() should return true for port locations"() {
     given:
-    Location originLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRRJK"), name: new InternationalizedName(name: "Rijeka"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-    )
-
-    Location destinationLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRZAD"), name: new InternationalizedName(name: "Zadar"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-    )
+    Location originLocation = Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----")
+    Location destinationLocation = Location.create("HRZAD", "Zadar", "Hrvatska", "1234----")
 
     when:
     Boolean canAccept = destinationLocation.canAcceptCargoFrom(originLocation)
@@ -92,15 +104,8 @@ class LocationSpecification extends Specification {
   @SuppressWarnings("GroovyPointlessBoolean")
   def "canAcceptCargoFrom() should return true for rail terminal locations"() {
     given:
-    Location originLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRRJK"), name: new InternationalizedName(name: "Rijeka"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1234----")
-    )
-
-    Location destinationLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRZAG"), name: new InternationalizedName(name: "Zagreb"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "-2345---")
-    )
+    Location originLocation = Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----")
+    Location destinationLocation = Location.create("HRZAG", "Zagreb", "Hrvatska", "-2345---")
 
     when:
     Boolean canAccept = destinationLocation.canAcceptCargoFrom(originLocation)
@@ -112,15 +117,8 @@ class LocationSpecification extends Specification {
   @SuppressWarnings("GroovyPointlessBoolean")
   def "canAcceptCargoFrom() should return false when locations cannot be connected as ports or as rail terminals"() {
     given:
-    Location originLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRKRK"), name: new InternationalizedName(name: "Krk"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "1-3-----")
-    )
-
-    Location destinationLocation = new Location(
-        unLoCode: new UnLoCode(code: "HRZAG"), name: new InternationalizedName(name: "Zagreb"), countryName: new InternationalizedName(name: "Hrvatska"),
-        unLoCodeFunction: new UnLoCodeFunction(functionEncoded: "-2345---")
-    )
+    Location originLocation = Location.create("HRKRK", "Krk", "Hrvatska", "1-3-----")
+    Location destinationLocation = Location.create("HRZAG", "Zagreb", "Hrvatska", "-2345---")
 
     when:
     Boolean canAccept = destinationLocation.canAcceptCargoFrom(originLocation)
