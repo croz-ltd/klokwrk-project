2d3deb001d3798ff44a9245f39d6a378771fa7ac
Damir Murat <damir.murat.git@gmail.com>
2020-04-28 19:28:20 +0200

    Rewording and expanding PostgreSqlCustomDialect javadoc.

--- infrastructure-library-hibernate/src/main/groovy/net/croz/cargotracker/infrastructure/library/hibernate/dialect/PostgreSqlAxonDialect.groovy
+++ infrastructure-library-hibernate/src/main/groovy/net/croz/cargotracker/infrastructure/library/hibernate/dialect/PostgreSqlAxonDialect.groovy
@@ -8,28 +8,42 @@ import org.hibernate.type.descriptor.sql.SqlTypeDescriptor
 import java.sql.Types
 
 /**
- * Customizes originally provided PostgreSQL dialect to align it with the Axon Event Store usage needs.
+ * Customizes originally provided PostgreSQL dialect to align it with the Axon's EventStore, and especially Axon's TokenStore, usage needs.
  * <p/>
  * In particular customizations are:
  * <ul>
  *   <li>inlined blob handling</li>
+ * </ul>
+ * <p/>
+ * <h3>Inlined blob handling</h3>
+ * By default, Hibernate PostgreSql dialect uses 'oid' data type for storing BLOB's. 'oid' data type is suitable for storing very large binary objects (MB or GB) in the space which is outside of
+ * user tables. Contrary, inlined blobs ('bytea' data type) should be better performing for small blobs (KB), and are stored directly in columns of user tables.
+ * <p/>
+ * In general, events stored in Axon event store should not be very big. At worst they will be few hundred KBs. Even when some of events are few MBs in size, 'bytea' should be good enough for storing
+ * and using them.
+ * <p/>
+ * If you are using Axon Server, than only the size of tracking token's BLOBs are concern (events are stored in Axon Server file-system based database). Since tracking tokens are very small (bellow
+ * 1 KB), the usage of 'bytea' data type is even more appropriate. Even Axon is using 'bytea' data type in its own implementations of JDBC stores.
+ * <p/>
+ * This dialect enforces the usage of PostgreSql's 'bytea' data type for storing BLOBs.
+ * <p/>
+ * For more information, there are few references around the web dealing with this problem:
+ * <ul>
+ *   <li>https://blog.trifork.com/2017/10/09/axon-postgresql-without-toast/</li>
+ *   <li>https://groups.google.com/forum/#!msg/axonframework/HhzQMbWfHTg/G04WbiixBAAJ</li>
+ *   <li>https://github.com/AxonIQ/reference-guide/issues/115</li>
  * </ul>
  */
 @SuppressWarnings("unused")
 @CompileStatic
 class PostgreSqlAxonDialect extends PostgreSQL10Dialect {
   PostgreSqlAxonDialect() {
+    super()
     registerColumnType(Types.BLOB, "bytea")
   }
 
   /**
-   * Together with registration of BLOB column type in constructor, causes PostgreSQL to use inline BLOBS (native postgre 'bytea' type) in tables instead of externalized ones (native postgre 'oid'
-   * type).
-   * <p/>
-   * Inline blobs should be better performing for small blobs (KB) while externalized ones are better for very large blobs (MB or GB). In our case (Axon Token store) blobs are bellow 1 KB so inline
-   * blobs are preferred.
-   * <p/>
-   * Usage of 'bytea' postgre type is consistent with types that Axon uses in theirs JDBC stores.
+   * Together with registration of BLOB column type in the constructor, causes PostgreSQL to use inline BLOBS ('bytea' type).
    */
   @Override
   SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
