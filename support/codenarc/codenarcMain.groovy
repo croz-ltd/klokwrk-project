0cc7876afa0841b2183af3fb7bfc9588766352b2
Damir Murat <damir.murat.git@gmail.com>
2020-05-01 20:00:03 +0200

    Configuring CodeNarc for projects with only basic rules included.

---
+++ support/codenarc/codenarcMain.groovy
@@ -0,0 +1,47 @@
+ruleset {
+
+  // rulesets/basic.xml
+  AssertWithinFinallyBlock
+  AssignmentInConditional
+  BigDecimalInstantiation
+  BitwiseOperatorInConditional
+  BooleanGetBoolean
+  BrokenNullCheck
+  BrokenOddnessCheck
+  ClassForName
+  ComparisonOfTwoConstants
+  ComparisonWithSelf
+  ConstantAssertExpression
+  ConstantIfExpression
+  ConstantTernaryExpression
+  DeadCode
+  DoubleNegative
+  DuplicateCaseStatement
+  DuplicateMapKey
+  DuplicateSetValue
+  EmptyCatchBlock
+  EmptyClass
+  EmptyElseBlock
+  EmptyFinallyBlock
+  EmptyForStatement
+  EmptyIfStatement
+  EmptyInstanceInitializer
+  EmptyMethod
+  EmptyStaticInitializer
+  EmptySwitchStatement
+  EmptySynchronizedStatement
+  EmptyTryBlock
+  EmptyWhileStatement
+  EqualsAndHashCode
+  EqualsOverloaded
+  ExplicitGarbageCollection
+  ForLoopShouldBeWhileLoop
+  HardCodedWindowsFileSeparator
+  HardCodedWindowsRootDirectory
+  IntegerGetInteger
+  MultipleUnaryOperators
+  RandomDoubleCoercedToZero
+  RemoveAllOnSelf
+  ReturnFromFinallyBlock
+  ThrowExceptionFromFinallyBlock
+}
