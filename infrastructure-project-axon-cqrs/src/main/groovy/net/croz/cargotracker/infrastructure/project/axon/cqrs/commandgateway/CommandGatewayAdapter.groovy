737e60ebe920990c25a93eeecc02bbb3f8f699d9
Damir Murat <damir.murat.git@gmail.com>
2020-05-03 22:36:47 +0200

    Adding javadoc for CommandGatewayAdapter.

--- infrastructure-project-axon-cqrs/src/main/groovy/net/croz/cargotracker/infrastructure/project/axon/cqrs/commandgateway/CommandGatewayAdapter.groovy
+++ infrastructure-project-axon-cqrs/src/main/groovy/net/croz/cargotracker/infrastructure/project/axon/cqrs/commandgateway/CommandGatewayAdapter.groovy
@@ -5,6 +5,9 @@ import org.axonframework.commandhandling.CommandExecutionException
 import org.axonframework.commandhandling.GenericCommandMessage
 import org.axonframework.commandhandling.gateway.CommandGateway
 
+/**
+ * Simplifies the API usage and exception handling of Axon <code>CommandGateway</code>.
+ */
 @CompileStatic
 class CommandGatewayAdapter {
   private final CommandGateway commandGateway
@@ -13,11 +16,33 @@ class CommandGatewayAdapter {
     this.commandGateway = commandGateway
   }
 
+  /**
+   * Delegates calls to the <code>CommandGateway.sendAndWait()</code> method with null metaData.
+   *
+   * @param command The command to dispatch.
+   * @param <R> The type of result expected from command execution.
+   * @return the result of command execution.
+   * @throws CommandExecutionException when details exception is not available.
+   * @throws Throwable when available as details of <code>CommandExecutionException</code>.
+   */
   @SuppressWarnings("GrUnnecessaryPublicModifier")
   public <R, C> R sendAndWait(C command) {
     sendAndWait(command, null)
   }
 
+  /**
+   * Delegates calls to the <code>CommandGateway.sendAndWait()</code> method.
+   * <p/>
+   * In case when an exception is thrown from <code>CommandGateway</code>, it unwraps details exception (if available), and rethrows it to the caller.
+   *
+   * @param command The command to dispatch.
+   * @param metaData The metadata map to dispatch with the command.
+   * @param <R> The type of result expected from command execution.
+   * @return the result of command execution.
+   * @throws AssertionError when command is null.
+   * @throws CommandExecutionException when details exception is not available.
+   * @throws Throwable when available as details of <code>CommandExecutionException</code>.
+   */
   @SuppressWarnings("GrUnnecessaryPublicModifier")
   public <R, C> R sendAndWait(C command, Map metaData) {
     assert command != null
