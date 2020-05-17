559e3ae0c241c9d25b33e222bbc07621be8e3755
Damir Murat <damir.murat.git@gmail.com>
2020-05-17 11:36:54 +0200

    Polishing - clarifying comment.

--- cargotracker/cargotracker-booking-commandside-app/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/cargobook/interfaces/web/CargoBookWebRequest.groovy
+++ cargotracker/cargotracker-booking-commandside-app/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/cargobook/interfaces/web/CargoBookWebRequest.groovy
@@ -3,7 +3,8 @@ package org.klokwrk.cargotracker.booking.commandside.cargobook.interfaces.web
 import groovy.transform.CompileStatic
 import org.klokwrk.cargotracker.booking.commandside.cargobook.boundary.api.conversation.CargoBookRequest
 
-// In general, in web interface we can use only CargoBookRequest, but CargoBookWebRequest can be used for adding additional properties that are only web specific.
+// In general, in web interface we use CargoBookRequest directly, but CargoBookWebRequest can be used for adding additional properties that are only web specific and handled in
+// controller before sending CargoBookRequest into domain facade.
 @CompileStatic
 class CargoBookWebRequest extends CargoBookRequest {
 }
--- cargotracker/cargotracker-booking-queryside-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/cargosummary/interfaces/web/CargoSummaryQueryWebRequest.groovy
+++ cargotracker/cargotracker-booking-queryside-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/cargosummary/interfaces/web/CargoSummaryQueryWebRequest.groovy
@@ -3,7 +3,8 @@ package org.klokwrk.cargotracker.booking.queryside.cargosummary.interfaces.web
 import groovy.transform.CompileStatic
 import org.klokwrk.cargotracker.booking.queryside.cargosummary.boundary.api.conversation.CargoSummaryQueryRequest
 
-// In general, in web interface we can use only CargoSummaryQueryRequest, but CargoSummaryQueryWebRequest can be used for adding additional properties that are only web specific.
+// In general, in web interface we use CargoSummaryQueryRequest directly, but CargoSummaryQueryWebRequest can be used for adding additional properties that are only web specific and handled in
+// controller before sending CargoSummaryQueryRequest into domain facade.
 @CompileStatic
 class CargoSummaryQueryWebRequest extends CargoSummaryQueryRequest {
 }
