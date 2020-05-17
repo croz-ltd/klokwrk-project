752b7eb00daef02cec7f6bcdc1f1ced18436fafc
Damir Murat <damir.murat.git@gmail.com>
2020-05-17 10:09:34 +0200

    Repackaging cargotracker-booking-queryside-rdbms-projection-app towards feature packaging.

--- cargotracker/cargotracker-booking-queryside-rdbms-projection-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/cargosummary/rdbms/projection/application/service/CargoSummaryProjectorService.groovy
+++ cargotracker/cargotracker-booking-queryside-rdbms-projection-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/cargosummary/rdbms/projection/application/service/CargoSummaryProjectorService.groovy
@@ -1,11 +1,11 @@
-package org.klokwrk.cargotracker.booking.queryside.rdbms.projection.application.service
+package org.klokwrk.cargotracker.booking.queryside.cargosummary.rdbms.projection.application.service
 
 import groovy.transform.CompileStatic
 import org.axonframework.eventhandling.DomainEventMessage
 import org.axonframework.eventhandling.EventHandler
 import org.klokwrk.cargotracker.booking.commandside.cargobook.axon.api.event.CargoBookedEvent
 import org.klokwrk.cargotracker.booking.queryside.rdbms.domain.querymodel.CargoSummaryQueryEntityRepository
-import org.klokwrk.cargotracker.booking.queryside.rdbms.projection.application.factory.CargoSummaryFactoryService
+import org.klokwrk.cargotracker.booking.queryside.cargosummary.rdbms.projection.application.factory.CargoSummaryFactoryService
 import org.springframework.stereotype.Service
 
 import javax.transaction.Transactional
