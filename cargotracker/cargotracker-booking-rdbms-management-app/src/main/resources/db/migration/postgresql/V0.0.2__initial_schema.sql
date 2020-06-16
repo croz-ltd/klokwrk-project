78ccb3061e9d5c46a5f572f752f0562343837e96
Damir Murat <damir.murat.git@gmail.com>
2020-06-16 13:35:51 +0200

    Syncing JPA entity with flyway schema.

--- cargotracker/cargotracker-booking-rdbms-management-app/src/main/resources/db/migration/postgresql/V0.0.2__initial_schema.sql
+++ cargotracker/cargotracker-booking-rdbms-management-app/src/main/resources/db/migration/postgresql/V0.0.2__initial_schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE token_entry (
-  processor_name CHARACTER VARYING(255) NOT NULL,
+  processor_name VARCHAR(255) NOT NULL,
   segment INTEGER NOT NULL,
-  OWNER CHARACTER VARYING(255),
-  TIMESTAMP CHARACTER VARYING(255) NOT NULL,
+  owner VARCHAR(255),
+  timestamp VARCHAR(255) NOT NULL,
   token BYTEA,
-  token_type CHARACTER VARYING(255),
+  token_type VARCHAR(255),
 
   PRIMARY KEY (processor_name, segment)
 );
@@ -12,13 +12,13 @@ CREATE TABLE token_entry (
 CREATE SEQUENCE cargo_summary_sequence INCREMENT BY 50 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 NO CYCLE;
 
 CREATE TABLE cargo_summary (
-  id BIGINT NOT NULL,
-  aggregate_identifier CHARACTER VARYING(255) NOT NULL,
+  id bigint NOT NULL,
+  aggregate_identifier CHAR(36) NOT NULL,
   aggregate_sequence_number BIGINT NOT NULL,
-  destination_location CHARACTER VARYING(255) NOT NULL,
-  inbound_channel_name CHARACTER VARYING(255) NOT NULL,
-  inbound_channel_type CHARACTER VARYING(255) NOT NULL,
-  origin_location CHARACTER VARYING(255) NOT NULL,
+  destination_location VARCHAR(255) NOT NULL,
+  inbound_channel_name VARCHAR(255) NOT NULL,
+  inbound_channel_type VARCHAR(255) NOT NULL,
+  origin_location VARCHAR(255) NOT NULL,
 
   PRIMARY KEY (id)
 );
