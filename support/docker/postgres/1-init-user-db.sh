c5bcc5796141a0ba13e8663085d9b5e4ec54d9f1
Damir Murat <damir.murat.git@gmail.com>
2020-06-21 15:01:09 +0200

    Renaming database in use and polishing names of environment variables.

--- support/docker/postgres/1-init-user-db.sh
+++ support/docker/postgres/1-init-user-db.sh
@@ -5,6 +5,6 @@ set -e
 psql -v ON_ERROR_STOP=1 <<-EOSQL
     CREATE USER cargotracker WITH PASSWORD 'cargotracker';
 
-    CREATE DATABASE cargo_tracker_query_database;
-    GRANT ALL PRIVILEGES ON DATABASE cargo_tracker_query_database TO cargotracker;
+    CREATE DATABASE cargotracker_booking_query_database;
+    GRANT ALL PRIVILEGES ON DATABASE cargotracker_booking_query_database TO cargotracker;
 EOSQL
