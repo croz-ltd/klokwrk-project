c4d291e574a8ad4264c258898b6c13204d61a13e
Damir Murat <damir.murat.git@gmail.com>
2020-06-29 15:23:22 +0200

    Externalizing image versions for dockerCompose* scripts.

--- support/docker/dockerComposeInfrastructureUp.sh
+++ support/docker/dockerComposeInfrastructureUp.sh
@@ -2,6 +2,18 @@
 
 serviceName=$1
 
+# ---------- Configuring environment variables based on values from gradle.properties in the root directory. - START
+function prop {
+  grep "${1}" ../../gradle.properties | cut -d '=' -f2 | sed 's/ //g'
+}
+
+postgreSqlDockerImageVersion=$(prop 'postgreSqlDockerImageVersion')
+export postgreSqlDockerImageVersion
+
+axonServerDockerImageVersion=$(prop 'axonServerDockerImageVersion')
+export axonServerDockerImageVersion
+# ---------- Configuring environment variables based on values from gradle.properties in the root directory. - END
+
 # shellcheck disable=SC2086
 #     - don't want quotes for variable here ("$serviceName") since they will be expanded into '' for empty variable.
 docker-compose --file docker-compose-infrastructure.yml --compatibility up --detach $serviceName
