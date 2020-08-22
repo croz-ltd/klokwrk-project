94ca2e1391c26d0ad6508e7c9701ec42b37230fc
Damir Murat <damir.murat.git@gmail.com>
2020-08-22 17:22:01 +0200

    Removing LocationSample from domain model into adapter layer.

--- cargotracker/cargotracker-booking-commandside-app/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/feature/cargobooking/adapter/out/remoting/InMemoryLocationRegistryService.groovy
+++ cargotracker/cargotracker-booking-commandside-app/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/feature/cargobooking/adapter/out/remoting/InMemoryLocationRegistryService.groovy
@@ -3,7 +3,6 @@ package org.klokwrk.cargotracker.booking.commandside.feature.cargobooking.adapte
 import groovy.transform.CompileStatic
 import org.klokwrk.cargotracker.booking.commandside.feature.cargobooking.application.port.out.FindLocationPortOut
 import org.klokwrk.cargotracker.booking.domain.model.Location
-import org.klokwrk.cargotracker.booking.domain.modelsample.LocationSample
 import org.springframework.stereotype.Service
 
 // TODO dmurat: implement real registry
@@ -13,5 +12,32 @@ class InMemoryLocationRegistryService implements FindLocationPortOut {
   Location findByUnLoCode(String unLoCode) {
     Location locationFound = LocationSample.findByUnLoCode(unLoCode)
     return locationFound
+  }
+
+  static class LocationSample {
+    @SuppressWarnings("DuplicateStringLiteral")
+    static final Map<String, Location> LOCATION_SAMPLE_MAP = [
+        "HRALJ": Location.create("HRALJ", "Aljmaš", "Hrvatska", "1-3-----"),
+        "HRBAK": Location.create("HRBAK", "Bakar", "Hrvatska", "123-----"),
+        "HRBAS": Location.create("HRBAS", "Baška", "Hrvatska", "1-3-----"),
+        "HRDBV": Location.create("HRDBV", "Dubrovnik", "Hrvatska", "1-34----"),
+        "HRDKO": Location.create("HRDKO", "Đakovo", "Hrvatska", "--3-----"),
+        "HRKOR": Location.create("HRKOR", "Korčula", "Hrvatska", "1-3-----"),
+        "HRKRK": Location.create("HRKRK", "Krk", "Hrvatska", "1-3-----"),
+        "HRMVN": Location.create("HRMVN", "Motovun", "Hrvatska", "--3-----"),
+        "HROSI": Location.create("HROSI", "Osijek", "Hrvatska", "1234----"),
+        "HRPUY": Location.create("HRPUY", "Pula", "Hrvatska", "1234----"),
+        "HRRJK": Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----"),
+        "HRSPU": Location.create("HRSPU", "Split", "Hrvatska", "1234----"),
+        "HRVUK": Location.create("HRVUK", "Vukovar", "Hrvatska", "123-5--B"),
+        "HRVZN": Location.create("HRVZN", "Varaždin", "Hrvatska", "-23-----"),
+        "HRZAD": Location.create("HRZAD", "Zadar", "Hrvatska", "1234----"),
+        "HRZAG": Location.create("HRZAG", "Zagreb", "Hrvatska", "-2345---")
+    ]
+
+    static Location findByUnLoCode(String unLoCode) {
+      Location locationFound = LOCATION_SAMPLE_MAP.get(unLoCode, Location.UNKNOWN_LOCATION)
+      return locationFound
+    }
   }
 }
--- cargotracker/cargotracker-booking-commandside-test-support/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/test/fixtures/feature/cargobooking/BookCargoCommandFixtures.groovy
+++ cargotracker/cargotracker-booking-commandside-test-support/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/test/fixtures/feature/cargobooking/BookCargoCommandFixtures.groovy
@@ -2,14 +2,19 @@ package org.klokwrk.cargotracker.booking.commandside.test.fixtures.feature.cargo
 
 import groovy.transform.CompileStatic
 import org.klokwrk.cargotracker.booking.axon.api.feature.cargobooking.command.BookCargoCommand
-
-import static org.klokwrk.cargotracker.booking.domain.modelsample.LocationSample.LOCATION_SAMPLE_MAP
+import org.klokwrk.cargotracker.booking.domain.model.Location
 
 /**
  * Contains test data fixtures for {@link BookCargoCommand}.
  */
 @CompileStatic
 class BookCargoCommandFixtures {
+  static final Map<String, Location> LOCATION_SAMPLE_MAP = [
+      "HRKRK": Location.create("HRKRK", "Krk", "Hrvatska", "1-3-----"),
+      "HRRJK": Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----"),
+      "HRZAG": Location.create("HRZAG", "Zagreb", "Hrvatska", "-2345---")
+  ]
+
   /**
    * Creates invalid command where origin and destination location are same.
    */
