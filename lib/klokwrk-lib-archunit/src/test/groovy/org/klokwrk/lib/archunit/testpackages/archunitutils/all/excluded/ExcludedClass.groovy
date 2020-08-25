038c83370d34671b119bb498dcdf7b2a4da42bc1
Damir Murat <damir.murat.git@gmail.com>
2020-08-25 10:41:57 +0200

    Adding tests for ArchUnitUtils.

---
+++ lib/klokwrk-lib-archunit/src/test/groovy/org/klokwrk/lib/archunit/testpackages/archunitutils/all/excluded/ExcludedClass.groovy
@@ -0,0 +1,7 @@
+package org.klokwrk.lib.archunit.testpackages.archunitutils.all.excluded
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class ExcludedClass {
+}
---
+++ lib/klokwrk-lib-archunit/src/test/groovy/org/klokwrk/lib/archunit/testpackages/archunitutils/all/included/IncludedClass.groovy
@@ -0,0 +1,7 @@
+package org.klokwrk.lib.archunit.testpackages.archunitutils.all.included
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class IncludedClass {
+}
