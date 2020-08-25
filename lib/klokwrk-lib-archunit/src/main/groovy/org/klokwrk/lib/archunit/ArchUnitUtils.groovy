6be89bc38c34784e818a47bca074ce246e45e0b5
Damir Murat <damir.murat.git@gmail.com>
2020-08-25 10:39:24 +0200

    Refactoring ArchUnitUtils methods for flexibility and testing.

--- lib/klokwrk-lib-archunit/src/main/groovy/org/klokwrk/lib/archunit/ArchUnitUtils.groovy
+++ lib/klokwrk-lib-archunit/src/main/groovy/org/klokwrk/lib/archunit/ArchUnitUtils.groovy
@@ -1,28 +1,25 @@
 package org.klokwrk.lib.archunit
 
-import com.tngtech.archunit.base.DescribedPredicate
-import com.tngtech.archunit.core.domain.JavaClass
 import com.tngtech.archunit.core.domain.JavaClasses
 import com.tngtech.archunit.core.importer.ClassFileImporter
 import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.core.importer.Location
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
+
+import java.util.regex.Pattern
 
 @Slf4j
 @CompileStatic
 class ArchUnitUtils {
-  static JavaClasses importJavaClassesFromPackages(Collection<String> packagesToImport, Collection<String> packagesToExclude = []) {
-    JavaClasses classesToImportUnfiltered = new ClassFileImporter()
-        .withImportOption(new ImportOption.DoNotIncludeTests())
-        .importPackages(packagesToImport)
-
-    JavaClasses classesToExclude = new ClassFileImporter()
-        .withImportOption(new ImportOption.DoNotIncludeTests())
-        .importPackages(packagesToExclude)
-
-    JavaClasses classesToImport = classesToImportUnfiltered
-        .that(new ExcludingPredicate(classesToExclude))
-        .as(classesToImportUnfiltered.description)
+  static JavaClasses importJavaClassesFromPackages(
+      Collection<String> packagesToImport,
+      Collection<String> packagesToExclude = [],
+      Collection<ImportOption> importOptions = [new ImportOption.DoNotIncludeTests()] as Collection<ImportOption>)
+  {
+    ClassFileImporter forImportClassFileImporter = new ClassFileImporter().withImportOption(new ExcludePackagesImportOption(packagesToExclude))
+    importOptions.each { ImportOption importOption -> forImportClassFileImporter = forImportClassFileImporter.withImportOption(importOption) }
+    JavaClasses classesToImport = forImportClassFileImporter.importPackages(packagesToImport)
 
     log.debug "---------- Following classes are imported:"
     classesToImport*.name.sort(false).each { String className -> log.debug(className) }
@@ -31,21 +28,23 @@ class ArchUnitUtils {
     return classesToImport
   }
 
-  private static class ExcludingPredicate extends DescribedPredicate<JavaClass> {
-    private final JavaClasses classesToExclude
+  private static class ExcludePackagesImportOption implements ImportOption {
+    Collection<String> packageToExcludeCollection
+    Collection<Pattern> patternToExcludeCollection
 
-    ExcludingPredicate(JavaClasses classesToExclude) {
-      super("excluding", [])
-      this.classesToExclude = classesToExclude
+    ExcludePackagesImportOption(Collection<String> packageToExcludeCollection) {
+      this.packageToExcludeCollection = packageToExcludeCollection
+      this.patternToExcludeCollection = []
+
+      this.packageToExcludeCollection.each { String packageToExclude ->
+        patternToExcludeCollection << Pattern.compile(/^.*${ packageToExclude.replace(".", "/") }.*\.class$/)
+      }
     }
 
     @Override
-    boolean apply(JavaClass inputJavaClass) {
-      if (classesToExclude.contain(inputJavaClass.name)) {
-        return false
-      }
-
-      return true
+    boolean includes(Location location) {
+      Boolean shouldExclude = patternToExcludeCollection.any { Pattern pattern -> location.matches(pattern) }
+      return !shouldExclude
     }
   }
 }
