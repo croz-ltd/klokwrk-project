1c681ae349aa85891b7879f7f1464d0891891538
Damir Murat <damir.murat.git@gmail.com>
2020-09-25 20:40:24 +0200

    Implementing the simplest possible Design-By-Contracts API through ContractsBase class.

---
+++ lang/klokwrk-lang-groovy/src/main/groovy/org/klokwrk/lang/groovy/contracts/base/ContractsBase.groovy
@@ -0,0 +1,29 @@
+package org.klokwrk.lang.groovy.contracts.base
+
+import groovy.transform.CompileStatic
+
+/**
+ * Defines base DBC (Design-by-Contracts) methods.
+ * <p/>
+ * Does not introduce any dependencies or Groovy AST magic. Methods are intended to be used directly. This is suitable when used from the libraries at the lowest language extension abstraction level
+ * since at that level we do not want to have any additional dependencies that are not absolutely necessary.
+ * <p/>
+ * At the abstraction levels that is higher than the lowest language extension abstraction level, there are other DBC modules/classes which might be more convenient and suitable, but usually
+ * introduce additional dependencies. These higher level DBC modules/classes will usually delegate its calls to this base DBC module/class.
+ */
+@CompileStatic
+class ContractsBase {
+  static final String REQUIRE_TRUE_MESSAGE_DEFAULT = "Require violation detected - boolean condition is false"
+
+  /**
+   * DBC precondition check based on boolean expression and optional message.
+   * </p>
+   * When condition evaluates to {@code false}, throws {@link AssertionError}.
+   */
+  static void requireTrueBase(Boolean condition, String message = "$REQUIRE_TRUE_MESSAGE_DEFAULT.") {
+    if (!condition) {
+      String myMessage = message?.trim() ?: "$REQUIRE_TRUE_MESSAGE_DEFAULT."
+      throw new AssertionError(myMessage as Object)
+    }
+  }
+}
