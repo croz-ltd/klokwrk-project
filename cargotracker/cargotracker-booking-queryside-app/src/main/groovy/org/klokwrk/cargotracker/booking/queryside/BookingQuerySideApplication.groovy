68d21dee5b0071eb7bb94ca973f60ba693ab2a93
Damir Murat <damir.murat.git@gmail.com>
2020-09-30 09:56:25 +0200

    Ignoring main methods in JaCoCo reports.

--- cargotracker/cargotracker-booking-queryside-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/BookingQuerySideApplication.groovy
+++ cargotracker/cargotracker-booking-queryside-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/BookingQuerySideApplication.groovy
@@ -1,6 +1,7 @@
 package org.klokwrk.cargotracker.booking.queryside
 
 import groovy.transform.CompileStatic
+import groovy.transform.Generated
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EntityScan(basePackages = ["org.klokwrk.cargotracker.booking.queryside.rdbms.projection.model"])
 @CompileStatic
 class BookingQuerySideApplication {
+  @Generated // Ignore in JaCoCo report as main method is not covered by JaCoCo (probably it is too early for JaCoCo to chip in)
   static void main(String[] args) {
     SpringApplication.run(BookingQuerySideApplication, args)
   }
--- cargotracker/cargotracker-booking-queryside-rdbms-projection-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/rdbms/projection/BookingQuerySideRdbmsProjectionApplication.groovy
+++ cargotracker/cargotracker-booking-queryside-rdbms-projection-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/rdbms/projection/BookingQuerySideRdbmsProjectionApplication.groovy
@@ -1,6 +1,7 @@
 package org.klokwrk.cargotracker.booking.queryside.rdbms.projection
 
 import groovy.transform.CompileStatic
+import groovy.transform.Generated
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EntityScan(basePackages = ["org.axonframework.eventhandling.tokenstore.jpa", "org.klokwrk.cargotracker.booking.queryside.rdbms.projection.model"])
 @CompileStatic
 class BookingQuerySideRdbmsProjectionApplication {
+  @Generated // Ignore in JaCoCo report as main method is not covered by JaCoCo (probably it is too early for JaCoCo to chip in)
   static void main(String[] args) {
     SpringApplication.run(BookingQuerySideRdbmsProjectionApplication, args)
   }
