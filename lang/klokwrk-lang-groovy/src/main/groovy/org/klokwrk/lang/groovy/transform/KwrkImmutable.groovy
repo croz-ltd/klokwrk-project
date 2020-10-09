0640a1ba4f1ac2c246f6fb3ccd500956a6d62432
Damir Murat <damir.murat.git@gmail.com>
2020-10-09 12:03:32 +0200

    Updating KwrkImmutable Groovydoc.

--- lang/klokwrk-lang-groovy/src/main/groovy/org/klokwrk/lang/groovy/transform/KwrkImmutable.groovy
+++ lang/klokwrk-lang-groovy/src/main/groovy/org/klokwrk/lang/groovy/transform/KwrkImmutable.groovy
@@ -17,7 +17,9 @@ import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Target
 
 /**
- * Variation of Groovy {@code Immutable} annotation that removes tuple constructor and uses {@link RelaxedPropertyHandler} instead of Groovy default {@code ImmutablePropertyHandler}.
+ * Variation of Groovy {@code Immutable} meta-annotation that removes tuple constructor and uses {@link RelaxedPropertyHandler} instead of Groovy default {@code ImmutablePropertyHandler}.
+ * <p/>
+ * In addition, it includes {@link KwrkMapConstructorNoArgHideable} and {@link KwrkMapConstructorDefaultPostCheck} annotations.
  * <p/>
  * All other options are same as in Groovy {@code Immutable}.
  */
