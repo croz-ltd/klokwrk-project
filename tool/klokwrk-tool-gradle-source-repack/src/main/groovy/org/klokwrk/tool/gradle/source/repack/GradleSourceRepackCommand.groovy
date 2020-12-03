b6c5b91d5d660e42c98d44be843c6515812e20e8
Damir Murat <damir.murat.git@gmail.com>
2020-12-03 18:43:53 +0100

    Adding klokwrk-tool-gradle-source-repack module.

---
+++ tool/klokwrk-tool-gradle-source-repack/src/main/groovy/org/klokwrk/tool/gradle/source/repack/GradleSourceRepackCommand.groovy
@@ -0,0 +1,9 @@
+package org.klokwrk.tool.gradle.source.repack
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class GradleSourceRepackCommand {
+  static void main(String[] args) throws Exception {
+  }
+}
---
+++ tool/klokwrk-tool-gradle-source-repack/src/test/groovy/org/klokwrk/tool/gradle/source/repack/GradleSourceRepackCommandSpecification.groovy
@@ -0,0 +1,11 @@
+package org.klokwrk.tool.gradle.source.repack
+
+import spock.lang.Specification
+
+class GradleSourceRepackCommandSpecification extends Specification {
+
+  void "test something"() {
+    expect:
+    true
+  }
+}
