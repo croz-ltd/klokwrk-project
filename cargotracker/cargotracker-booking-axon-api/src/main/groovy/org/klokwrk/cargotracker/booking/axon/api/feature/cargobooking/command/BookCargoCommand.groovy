35f11f8562dc379ce8276442017a5f650afa57ea
Damir Murat <damir.murat.git@gmail.com>
2021-03-29 20:56:27 +0200

    Make Axon commands immutable.

--- cargotracker/cargotracker-booking-axon-api/src/main/groovy/org/klokwrk/cargotracker/booking/axon/api/feature/cargobooking/command/BookCargoCommand.groovy
+++ cargotracker/cargotracker-booking-axon-api/src/main/groovy/org/klokwrk/cargotracker/booking/axon/api/feature/cargobooking/command/BookCargoCommand.groovy
@@ -18,19 +18,15 @@
 package org.klokwrk.cargotracker.booking.axon.api.feature.cargobooking.command
 
 import groovy.transform.CompileStatic
-import groovy.transform.MapConstructor
-import groovy.transform.PropertyOptions
 import org.klokwrk.cargotracker.booking.domain.model.Location
 import org.klokwrk.cargotracker.lib.axon.api.command.BaseCreateCommand
-import org.klokwrk.lang.groovy.transform.options.RelaxedPropertyHandler
+import org.klokwrk.lang.groovy.transform.KwrkImmutable
 
-@PropertyOptions(propertyHandler = RelaxedPropertyHandler)
-@MapConstructor(noArg = true)
+@KwrkImmutable
 @CompileStatic
 class BookCargoCommand implements BaseCreateCommand {
-  // TODO dmurat: immutability - commands probably should/can be immutable. Implement immutability and post construction rules for properties.
-
   String aggregateIdentifier
+
   Location originLocation
   Location destinationLocation
 }
