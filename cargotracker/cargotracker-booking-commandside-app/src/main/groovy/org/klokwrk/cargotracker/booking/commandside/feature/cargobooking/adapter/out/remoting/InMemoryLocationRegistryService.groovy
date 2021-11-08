5dcf06fe93771eb6b83d73aadc34cc200ee649b5
Damir Murat <damir.murat.git@gmail.com>
2021-11-08 11:04:32 +0100

    Remove the majority of Croatian locations
    Leave in place locations used in integration and component tests.

--- cargotracker/cargotracker-booking-commandside-app/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/feature/cargobooking/adapter/out/remoting/InMemoryLocationRegistryService.groovy
+++ cargotracker/cargotracker-booking-commandside-app/src/main/groovy/org/klokwrk/cargotracker/booking/commandside/feature/cargobooking/adapter/out/remoting/InMemoryLocationRegistryService.groovy
@@ -34,21 +34,8 @@ class InMemoryLocationRegistryService implements FindLocationPortOut {
   static class LocationSample {
     @SuppressWarnings("CodeNarc.DuplicateStringLiteral")
     static final Map<String, Location> LOCATION_SAMPLE_MAP = [
-        "HRALJ": Location.create("HRALJ", "Aljmaš", "Hrvatska", "1-3-----"),
-        "HRBAK": Location.create("HRBAK", "Bakar", "Hrvatska", "123-----"),
-        "HRBAS": Location.create("HRBAS", "Baška", "Hrvatska", "1-3-----"),
-        "HRDBV": Location.create("HRDBV", "Dubrovnik", "Hrvatska", "1-34----"),
-        "HRDKO": Location.create("HRDKO", "Đakovo", "Hrvatska", "--3-----"),
-        "HRKOR": Location.create("HRKOR", "Korčula", "Hrvatska", "1-3-----"),
         "HRKRK": Location.create("HRKRK", "Krk", "Hrvatska", "1-3-----"),
-        "HRMVN": Location.create("HRMVN", "Motovun", "Hrvatska", "--3-----"),
-        "HROSI": Location.create("HROSI", "Osijek", "Hrvatska", "1234----"),
-        "HRPUY": Location.create("HRPUY", "Pula", "Hrvatska", "1234----"),
         "HRRJK": Location.create("HRRJK", "Rijeka", "Hrvatska", "1234----"),
-        "HRSPU": Location.create("HRSPU", "Split", "Hrvatska", "1234----"),
-        "HRVUK": Location.create("HRVUK", "Vukovar", "Hrvatska", "123-5--B"),
-        "HRVZN": Location.create("HRVZN", "Varaždin", "Hrvatska", "-23-----"),
-        "HRZAD": Location.create("HRZAD", "Zadar", "Hrvatska", "1234----"),
         "HRZAG": Location.create("HRZAG", "Zagreb", "Hrvatska", "-2345---")
     ]
 
