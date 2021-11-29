9c29e28afd92ff9cb1ca95b2eddf7cf288af138d
Damir Murat <damir.murat.git@gmail.com>
2021-11-29 20:51:01 +0100

    fix: Fixing DB privileges in init script

--- support/docker/postgres/1-init-user-db.sh
+++ support/docker/postgres/1-init-user-db.sh
@@ -8,6 +8,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 <<-EOSQL
     CREATE USER cargotracker WITH PASSWORD 'cargotracker';
+    ALTER USER cargotracker WITH CREATEROLE;
 
     CREATE DATABASE cargotracker_booking_query_database;
     GRANT ALL PRIVILEGES ON DATABASE cargotracker_booking_query_database TO cargotracker;
