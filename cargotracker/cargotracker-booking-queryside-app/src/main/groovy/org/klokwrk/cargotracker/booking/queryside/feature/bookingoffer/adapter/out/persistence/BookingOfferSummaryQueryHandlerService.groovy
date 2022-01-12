27ff43ee597d8f8461eb25e5da1cc1a4c1539c6d
Damir Murat <damir.murat.git@gmail.com>
2022-01-12 14:18:43 +0100

    Rename package "cargoinfo" to "bookingoffer"

---
+++ cargotracker/cargotracker-booking-queryside-app/src/main/groovy/org/klokwrk/cargotracker/booking/queryside/feature/bookingoffer/adapter/out/persistence/BookingOfferSummaryQueryHandlerService.groovy
@@ -0,0 +1,61 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020-2022 CROZ d.o.o, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.klokwrk.cargotracker.booking.queryside.feature.bookingoffer.adapter.out.persistence
+
+import groovy.transform.CompileStatic
+import org.axonframework.queryhandling.QueryHandler
+import org.klokwrk.cargotracker.booking.queryside.feature.bookingoffer.application.port.in.BookingOfferSummaryQueryRequest
+import org.klokwrk.cargotracker.booking.queryside.feature.bookingoffer.application.port.in.BookingOfferSummaryQueryResponse
+import org.klokwrk.cargotracker.booking.queryside.rdbms.projection.model.BookingOfferSummaryJpaEntity
+import org.klokwrk.cargotracker.booking.queryside.rdbms.projection.model.BookingOfferSummaryJpaRepository
+import org.klokwrk.cargotracker.lib.boundary.api.domain.exception.QueryException
+import org.klokwrk.cargotracker.lib.boundary.api.domain.violation.ViolationInfo
+import org.springframework.stereotype.Service
+
+/**
+ * Implements query handling related to booking offer summary.
+ * <p/>
+ * Do note that there is no point for marking this class/method with <code>Transactional</code> annotations. Transaction is under control of Axon's <code>SpringTransactionManager</code> which does
+ * not supports <code>Transactional</code> annotation. Rather <code>SpringTransactionManager</code> uses a single <code>TransactionDefinition</code> for all its message handlers.
+ * <p/>
+ * This is unfortunate since we are loosing capability to specify transaction attributes per class or method, and readOnly attribute will be quite nice to have. Fortunately, since this (and others)
+ * query handler runs in standalone application, we can resolve this by configuring "global" Axon's transaction definition to be read-only.
+ * <p/>
+ * For more information and some resources, take a look at <code>axonTransactionManager</code> bean in <code>SpringBootConfig</code> class.
+ */
+@Service
+@CompileStatic
+class BookingOfferSummaryQueryHandlerService {
+  private final BookingOfferSummaryJpaRepository bookingOfferSummaryJpaRepository
+
+  BookingOfferSummaryQueryHandlerService(BookingOfferSummaryJpaRepository bookingOfferSummaryJpaRepository) {
+    this.bookingOfferSummaryJpaRepository = bookingOfferSummaryJpaRepository
+  }
+
+  @QueryHandler
+  BookingOfferSummaryQueryResponse handleBookingOfferSummaryQueryRequest(BookingOfferSummaryQueryRequest bookingOfferSummaryQueryRequest) {
+    BookingOfferSummaryJpaEntity bookingOfferSummaryJpaEntity = bookingOfferSummaryJpaRepository.findByBookingOfferIdentifier(bookingOfferSummaryQueryRequest.bookingOfferIdentifier)
+
+    if (!bookingOfferSummaryJpaEntity) {
+      throw new QueryException(ViolationInfo.NOT_FOUND)
+    }
+
+    BookingOfferSummaryQueryResponse bookingOfferSummaryQueryResponse = new BookingOfferSummaryQueryResponse(bookingOfferSummaryJpaEntity.properties)
+    return bookingOfferSummaryQueryResponse
+  }
+}
