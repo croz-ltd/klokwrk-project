a531eb4eded55ccc7c728280ab07eee13914e3db
Damir Murat <damir.murat.git@gmail.com>
2022-03-23 11:04:23 +0100

    fix: Add workaround for a bug in docker-compose v2.3.3

--- support/docker/dockerComposeRdbmsMigration.sh
+++ support/docker/dockerComposeRdbmsMigration.sh
@@ -9,6 +9,10 @@ cargotrackerBookingRdbmsManagementAppDockerImageVersion=$(prop 'cargotrackerBook
 export cargotrackerBookingRdbmsManagementAppDockerImageVersion
 # ---------- Configuring environment variables based on values from gradle.properties in the root directory. - END
 
-docker-compose --file docker-compose-rdbms-migration.yml up
+# docker-compose --file docker-compose-rdbms-migration.yml up
 
-docker container rm cargotracker-booking-rdbms-management-app
+# docker container rm cargotracker-booking-rdbms-management-app
+
+### Starting with docker-compose of version 2.3.3 `docker-compose up` command above does not return to the terminal after container exits. I believe it is a bug. Until fixed, the `run` command
+### bellow can be used as an alternative
+docker-compose --file docker-compose-rdbms-migration.yml run --rm cargotracker-booking-rdbms-management-app
