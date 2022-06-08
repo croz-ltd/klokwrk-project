5be9352629f6b7e654ad9bc21d48b912e4e01656
Damir Murat <damir.murat.git@gmail.com>
2022-06-08 16:19:33 +0200

    Polishing - harmonize database types casing in DDLs

--- cargotracker/cargotracker-booking-rdbms-management-app/src/main/resources/db/migration/postgresql/V0.0.1__initial_schema.sql
+++ cargotracker/cargotracker-booking-rdbms-management-app/src/main/resources/db/migration/postgresql/V0.0.1__initial_schema.sql
@@ -17,12 +17,12 @@
 --
 
 CREATE TABLE token_entry (
-  processor_name TEXT NOT NULL,
-  segment INTEGER NOT NULL,
-  owner TEXT,
-  timestamp TEXT NOT NULL,
-  token BYTEA,
-  token_type TEXT,
+  processor_name text NOT NULL,
+  segment integer NOT NULL,
+  owner text,
+  timestamp text NOT NULL,
+  token bytea,
+  token_type text,
 
   PRIMARY KEY (processor_name, segment)
 );
@@ -32,18 +32,18 @@ CREATE USER cargotracker_readonly WITH PASSWORD 'cargotracker_readonly';
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO cargotracker_readonly;
 
 CREATE TABLE booking_offer_summary (
-  booking_offer_identifier UUID PRIMARY KEY,
+  booking_offer_identifier uuid PRIMARY KEY,
 
-  customer_identifier TEXT NOT NULL,
-  customer_type TEXT NOT NULL,
+  customer_identifier text NOT NULL,
+  customer_type text NOT NULL,
 
-  origin_location_un_lo_code TEXT NOT NULL,
-  origin_location_name TEXT NOT NULL,
-  origin_location_country_name TEXT NOT NULL,
+  origin_location_un_lo_code text NOT NULL,
+  origin_location_name text NOT NULL,
+  origin_location_country_name text NOT NULL,
 
-  destination_location_un_lo_code TEXT NOT NULL,
-  destination_location_name TEXT NOT NULL,
-  destination_location_country_name TEXT NOT NULL,
+  destination_location_un_lo_code text NOT NULL,
+  destination_location_name text NOT NULL,
+  destination_location_country_name text NOT NULL,
 
   departure_earliest_time timestamptz NOT NULL,
   departure_latest_time timestamptz NOT NULL,
@@ -53,10 +53,10 @@ CREATE TABLE booking_offer_summary (
   commodity_total_weight_kg integer NOT NULL,
   commodity_total_container_teu_count numeric(8, 2) NOT NULL,
 
-  inbound_channel_name TEXT NOT NULL,
-  inbound_channel_type TEXT NOT NULL,
+  inbound_channel_name text NOT NULL,
+  inbound_channel_type text NOT NULL,
 
   first_event_recorded_at timestamptz NOT NULL,
   last_event_recorded_at timestamptz NOT NULL,
-  last_event_sequence_number BIGINT NOT NULL
+  last_event_sequence_number bigint NOT NULL
 );
