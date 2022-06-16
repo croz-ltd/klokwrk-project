22840021208839fdc9ad73116b04950bf5a7b962
Damir Murat <damir.murat.git@gmail.com>
2022-06-16 13:09:19 +0200

    Add Hibernate-specific operations for Spring Data JPA repositories

---
+++ cargotracker/cargotracker-booking-queryside-rdbms-projection-model/src/main/groovy/org/klokwrk/lib/springframework/data/jpa/repository/hibernate/HibernateJpaRepository.groovy
@@ -0,0 +1,42 @@
+package org.klokwrk.lib.springframework.data.jpa.repository.hibernate
+
+import groovy.transform.CompileStatic
+
+/**
+ * This interface is intended to add Hibernate-specific methods into Spring Data JPA repositories to resolve some Spring Data JPA implementation inefficiencies.
+ * <p/>
+ * The main problem with Spring Data JPA stems from the generic implementation of {@code saveXXX()} methods because they are falling back to the EntityManager's {@code mergeXXX()} methods for
+ * entities with assigned identifiers.
+ * <p/>
+ * Note: since this interface and the corresponding implementation are expected to be published as part of the {@code hibernate-types library}, I'm not performing complete testing here. So instead, I
+ * will just replace it with the published version in the future.
+ * <p/>
+ * Reference: https://vladmihalcea.com/best-spring-data-jparepository/
+ */
+// TODO dmurat: Replace with hibernate-types library once Spring Data JPA Hibernate optimized repository implementation is published (https://github.com/vladmihalcea/hibernate-types)
+@CompileStatic
+interface HibernateJpaRepository<T> {
+  // Save methods will trigger an UnsupportedOperationException
+  @Deprecated <S extends T> S save(S entity)
+  @Deprecated <S extends T> List<S> saveAll(Iterable<S> entities)
+  @Deprecated <S extends T> S saveAndFlush(S entity)
+  @Deprecated <S extends T> List<S> saveAllAndFlush(Iterable<S> entities)
+
+  // Persist methods are meant to save newly created entities
+  <S extends T> S persist(S entity)
+  <S extends T> S persistAndFlush(S entity)
+  <S extends T> List<S> persistAll(Iterable<S> entities)
+  <S extends T> List<S> persistAllAndFlush(Iterable<S> entities)
+
+  // Merge methods are meant to propagate detached entity state changes if they are really needed
+  <S extends T> S merge(S entity)
+  <S extends T> S mergeAndFlush(S entity)
+  <S extends T> List<S> mergeAll(Iterable<S> entities)
+  <S extends T> List<S> mergeAllAndFlush(Iterable<S> entities)
+
+  //Update methods are meant to force the detached entity state changes
+  <S extends T> S update(S entity)
+  <S extends T> S updateAndFlush(S entity)
+  <S extends T> List<S> updateAll(Iterable<S> entities)
+  <S extends T> List<S> updateAllAndFlush(Iterable<S> entities)
+}
