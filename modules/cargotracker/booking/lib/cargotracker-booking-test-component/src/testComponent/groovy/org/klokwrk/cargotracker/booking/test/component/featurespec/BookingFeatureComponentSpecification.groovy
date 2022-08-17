1c47955ee1ee77883272f666b599e61be76cae9f
Damir Murat <damir.murat.git@gmail.com>
2022-08-17 09:34:26 +0200

    Add component tests for findAll and searchAll queries

--- modules/cargotracker/booking/lib/cargotracker-booking-test-component/src/testComponent/groovy/org/klokwrk/cargotracker/booking/test/component/featurespec/BookingFeatureComponentSpecification.groovy
+++ modules/cargotracker/booking/lib/cargotracker-booking-test-component/src/testComponent/groovy/org/klokwrk/cargotracker/booking/test/component/featurespec/BookingFeatureComponentSpecification.groovy
@@ -26,15 +26,52 @@ import org.klokwrk.cargotracker.booking.test.component.test.base.AbstractCompone
 import java.time.Duration
 import java.time.Instant
 
+import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeCommandRequestBodyList_createBookingOffer
 import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeCommandRequestBody_createBookingOffer_chilledCommodity
 import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeCommandRequestBody_createBookingOffer_dryCommodity
 import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeCommandRequestBody_createBookingOffer_invalid
 import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeCommandRequestUrl_createBookingOffer
+import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeQueryRequestBody_bookingOfferSummary_findAll_standardCustomer
 import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeQueryRequestBody_bookingOfferSummary_findById
+import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeQueryRequestBody_bookingOfferSummary_searchAll
+import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeQueryRequestUrl_bookingOfferSummary_findAll
 import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeQueryRequestUrl_bookingOfferSummary_findById
+import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeQueryRequestUrl_bookingOfferSummary_searchAll
 import static org.klokwrk.cargotracker.booking.test.component.test.util.FeatureTestHelpers.makeRequest
 
 class BookingFeatureComponentSpecification extends AbstractComponentSpecification {
+  @SuppressWarnings("CodeNarc.PropertyName")
+  static Integer countOf_createdBookingOffers_forStandardCustomer = 0
+
+  void setupSpec() {
+    // Execute a list of commands with predefined data used for findAll and searchAll queries
+    List<String> commandRequestBodyList = makeCommandRequestBodyList_createBookingOffer()
+    List<String> createdBookingOfferIdentifierList = []
+    commandRequestBodyList.each { String commandRequestBody ->
+      Request commandRequest = makeRequest(makeCommandRequestUrl_createBookingOffer(commandSideApp), commandRequestBody, "en")
+      HttpResponse commandResponse = commandRequest.execute().returnResponse()
+      assert commandResponse.statusLine.statusCode == 200
+
+      if (commandRequestBody.contains("standard-customer@cargotracker.com")) {
+        countOf_createdBookingOffers_forStandardCustomer++
+      }
+
+      Object commandResponseJson = new JsonSlurper().parseText(commandResponse.entity.content.text)
+      String commandResponseBookingOfferIdentifier = commandResponseJson.payload.bookingOfferId.identifier
+      createdBookingOfferIdentifierList << commandResponseBookingOfferIdentifier
+    }
+
+    // Wait for projection of a event corresponding to the last command
+    Request queryRequest = makeRequest(
+        makeQueryRequestUrl_bookingOfferSummary_findById(querySideViewApp), makeQueryRequestBody_bookingOfferSummary_findById(createdBookingOfferIdentifierList.last()), "en"
+    )
+    Awaitility.await().atMost(Duration.ofSeconds(5)).until({
+      HttpResponse queryResponse = queryRequest.execute().returnResponse()
+      Integer queryResponseStatusCode = queryResponse.statusLine.statusCode
+      queryResponseStatusCode == 200
+    })
+  }
+
   void "command - createBookingOffer - should create booking offer: [acceptLanguageHeader: #acceptLanguageHeaderParam]"() {
     given:
     Request commandRequest = makeRequest(makeCommandRequestUrl_createBookingOffer(commandSideApp), commandBodyParam as String, acceptLanguageHeaderParam as String)
@@ -97,7 +134,8 @@ class BookingFeatureComponentSpecification extends AbstractComponentSpecificatio
 
     when:
     Request queryRequest = makeRequest(
-        makeQueryRequestUrl_bookingOfferSummary_findById(querySideViewApp), makeQueryRequestBody_bookingOfferSummary_findById(commandResponseBookingOfferIdentifier), acceptLanguageHeaderParam as String
+        makeQueryRequestUrl_bookingOfferSummary_findById(querySideViewApp), makeQueryRequestBody_bookingOfferSummary_findById(commandResponseBookingOfferIdentifier),
+        acceptLanguageHeaderParam as String
     )
 
     HttpResponse queryResponse = null
@@ -165,5 +203,125 @@ class BookingFeatureComponentSpecification extends AbstractComponentSpecificatio
     acceptLanguageHeaderParam | violationMessageParam
     "hr-HR"                   | "Sumarni izvještaj za željenu ponudu za rezervaciju nije pronađen."
     "en"                      | "Summary report for specified booking offer is not found."
+  }
+
+  void "query - bookingOfferSummary_findAll - should find existing booking offers with default paging and sorting"() {
+    given:
+    Request queryRequest = makeRequest(makeQueryRequestUrl_bookingOfferSummary_findAll(querySideViewApp), makeQueryRequestBody_bookingOfferSummary_findAll_standardCustomer(), "en")
+
+    when:
+    HttpResponse queryResponse = queryRequest.execute().returnResponse()
+    Integer queryResponseStatusCode = queryResponse.statusLine.statusCode
+    assert queryResponseStatusCode == 200
+
+    Map queryResponseJson = new JsonSlurper().parseText(queryResponse.entity.content.text) as Map
+
+    then:
+    verifyAll(queryResponseJson.metaData as Map) {
+      size() == 2
+
+      verifyAll(general as Map) {
+        size() == 3
+        locale == "en"
+        severity == "info"
+        timestamp
+      }
+
+      verifyAll(http as Map) {
+        size() == 2
+        message == "OK"
+        status == "200"
+      }
+    }
+
+    verifyAll(queryResponseJson.payload as Map, {
+      size() == 2
+
+      verifyAll(pageInfo as Map, {
+        pageOrdinal == 0
+        pageElementsCount >= this.countOf_createdBookingOffers_forStandardCustomer
+        first == true
+        totalElementsCount >= pageElementsCount
+
+        verifyAll(requestedPageRequirement as Map) {
+          size() == 2
+          ordinal == 0
+          size == 25
+        }
+
+        verifyAll((it.requestedSortRequirementList as List)[0] as Map) {
+          size() == 2
+          propertyName == "lastEventRecordedAt"
+          direction == "DESC"
+        }
+      })
+
+      verifyAll(pageContent as List<Map>, {
+        size() >= this.countOf_createdBookingOffers_forStandardCustomer
+        it.every({ Map pageElement -> pageElement.customerType == "STANDARD" })
+        it.every({ Map pageElement -> pageElement.lastEventSequenceNumber == 0 })
+      })
+    })
+  }
+
+//  @SuppressWarnings("CodeNarc.UnnecessaryObjectReferences")
+  void "query - bookingOfferSummary_searchAll - should find existing booking offers with default paging and sorting"() {
+    given:
+    Request queryRequest = makeRequest(makeQueryRequestUrl_bookingOfferSummary_searchAll(querySideViewApp), makeQueryRequestBody_bookingOfferSummary_searchAll(), "en")
+
+    when:
+    HttpResponse queryResponse = queryRequest.execute().returnResponse()
+    Integer queryResponseStatusCode = queryResponse.statusLine.statusCode
+    assert queryResponseStatusCode == 200
+
+    Map queryResponseJson = new JsonSlurper().parseText(queryResponse.entity.content.text) as Map
+
+    then:
+    verifyAll(queryResponseJson.metaData as Map) {
+      size() == 2
+
+      verifyAll(general as Map) {
+        size() == 3
+        locale == "en"
+        severity == "info"
+        timestamp
+      }
+
+      verifyAll(http as Map) {
+        size() == 2
+        message == "OK"
+        status == "200"
+      }
+    }
+
+    verifyAll(queryResponseJson.payload as Map, {
+      size() == 2
+
+      verifyAll(pageInfo as Map, {
+        pageOrdinal == 0
+        first == true
+
+        verifyAll(requestedPageRequirement as Map) {
+          size() == 2
+          ordinal == 0
+          size == 25
+        }
+
+        verifyAll((it.requestedSortRequirementList as List)[0] as Map) {
+          size() == 2
+          propertyName == "lastEventRecordedAt"
+          direction == "DESC"
+        }
+      })
+
+      verifyAll(pageContent as List<Map>, {
+        size() >= 2
+        it.every({ Map pageElement -> pageElement.customerType == "STANDARD" })
+        it.every({ Map pageElement -> pageElement.originLocationName == "Rijeka" })
+        it.every({ Map pageElement -> pageElement.destinationLocationCountryName == "The United States of America" })
+        it.every({ Map pageElement -> pageElement.commodityTotalWeightKg >= 15000 && pageElement.commodityTotalWeightKg <= 100000 })
+        it.every({ Map pageElement -> pageElement.lastEventSequenceNumber == 0 })
+      })
+    })
   }
 }
--- modules/cargotracker/booking/lib/cargotracker-booking-test-component/src/testComponent/groovy/org/klokwrk/cargotracker/booking/test/component/test/util/FeatureTestHelpers.groovy
+++ modules/cargotracker/booking/lib/cargotracker-booking-test-component/src/testComponent/groovy/org/klokwrk/cargotracker/booking/test/component/test/util/FeatureTestHelpers.groovy
@@ -22,6 +22,18 @@ class FeatureTestHelpers {
     return bookingOfferSummaryQueryUrl
   }
 
+  static String makeQueryRequestUrl_bookingOfferSummary_findAll(GenericContainer querySideViewApp) {
+    //noinspection HttpUrlsUsage
+    String bookingOfferSummaryQueryUrl = "http://${ querySideViewApp.host }:${ querySideViewApp.firstMappedPort }/cargotracker-booking-queryside-view/booking-offer/booking-offer-summary-find-all"
+    return bookingOfferSummaryQueryUrl
+  }
+
+  static String makeQueryRequestUrl_bookingOfferSummary_searchAll(GenericContainer querySideViewApp) {
+    //noinspection HttpUrlsUsage
+    String bookingOfferSummaryQueryUrl = "http://${ querySideViewApp.host }:${ querySideViewApp.firstMappedPort }/cargotracker-booking-queryside-view/booking-offer/booking-offer-summary-search-all"
+    return bookingOfferSummaryQueryUrl
+  }
+
   static String makeCommandRequestBody_createBookingOffer_dryCommodity(Instant baseTime = Instant.now()) {
     Instant departureEarliestTime = baseTime + Duration.ofHours(1)
     Instant departureLatestTime = baseTime + Duration.ofHours(2)
@@ -104,11 +116,176 @@ class FeatureTestHelpers {
     return commandRequestBody
   }
 
+  @SuppressWarnings("CodeNarc.MethodSize")
+  static List<String> makeCommandRequestBodyList_createBookingOffer() {
+    Instant currentTime = Instant.now()
+    Instant departureEarliestTime = currentTime + Duration.ofHours(1)
+    Instant departureLatestTime = currentTime + Duration.ofHours(2)
+    Instant arrivalLatestTime = currentTime + Duration.ofHours(3)
+
+    List<String> commandRequestBodyList = []
+    commandRequestBodyList << """
+        {
+          "userIdentifier": "standard-customer@cargotracker.com",
+          "routeSpecification": {
+            "originLocation": "HRRJK",
+            "destinationLocation": "NLRTM",
+            "departureEarliestTime": "${ departureEarliestTime }",
+            "departureLatestTime": "${ departureLatestTime }",
+            "arrivalLatestTime": "${ arrivalLatestTime }"
+          },
+          "commodityInfo": {
+            "commodityType": "dry",
+            "totalWeightInKilograms": 30000
+          },
+          "containerDimensionType": "DIMENSION_ISO_22"
+        }
+        """.toString()
+
+    commandRequestBodyList << """
+        {
+          "userIdentifier": "gold-customer@cargotracker.com",
+          "routeSpecification": {
+            "originLocation": "DEHAM",
+            "destinationLocation": "USLAX",
+            "departureEarliestTime": "${ departureEarliestTime }",
+            "departureLatestTime": "${ departureLatestTime }",
+            "arrivalLatestTime": "${ arrivalLatestTime }"
+          },
+          "commodityInfo": {
+            "commodityType": "air_cooled",
+            "totalWeightInKilograms": 70000
+          },
+          "containerDimensionType": "DIMENSION_ISO_22"
+        }
+        """.toString()
+
+    commandRequestBodyList << """
+        {
+          "userIdentifier": "platinum-customer@cargotracker.com",
+          "routeSpecification": {
+            "originLocation": "NLRTM",
+            "destinationLocation": "USNYC",
+            "departureEarliestTime": "${ departureEarliestTime }",
+            "departureLatestTime": "${ departureLatestTime }",
+            "arrivalLatestTime": "${ arrivalLatestTime }"
+          },
+          "commodityInfo": {
+            "commodityType": "dry",
+            "totalWeightInKilograms": 40000
+          },
+          "containerDimensionType": "DIMENSION_ISO_22"
+        }
+        """.toString()
+
+    commandRequestBodyList << """
+        {
+          "userIdentifier": "standard-customer@cargotracker.com",
+          "routeSpecification": {
+            "originLocation": "HRRJK",
+            "destinationLocation": "DEHAM",
+            "departureEarliestTime": "${ departureEarliestTime }",
+            "departureLatestTime": "${ departureLatestTime }",
+            "arrivalLatestTime": "${ arrivalLatestTime }"
+          },
+          "commodityInfo": {
+            "commodityType": "dry",
+            "totalWeightInKilograms": 1000
+          },
+          "containerDimensionType": "DIMENSION_ISO_22"
+        }
+        """.toString()
+
+    commandRequestBodyList << """
+        {
+          "userIdentifier": "standard-customer@cargotracker.com",
+          "routeSpecification": {
+            "originLocation": "HRRJK",
+            "destinationLocation": "USLAX",
+            "departureEarliestTime": "${ departureEarliestTime }",
+            "departureLatestTime": "${ departureLatestTime }",
+            "arrivalLatestTime": "${ arrivalLatestTime }"
+          },
+          "commodityInfo": {
+            "commodityType": "dry",
+            "totalWeightInKilograms": 15000
+          },
+          "containerDimensionType": "DIMENSION_ISO_22"
+        }
+        """.toString()
+
+    commandRequestBodyList << """
+        {
+          "userIdentifier": "standard-customer@cargotracker.com",
+          "routeSpecification": {
+            "originLocation": "HRRJK",
+            "destinationLocation": "USNYC",
+            "departureEarliestTime": "${ departureEarliestTime }",
+            "departureLatestTime": "${ departureLatestTime }",
+            "arrivalLatestTime": "${ arrivalLatestTime }"
+          },
+          "commodityInfo": {
+            "commodityType": "dry",
+            "totalWeightInKilograms": 100000
+          },
+          "containerDimensionType": "DIMENSION_ISO_22"
+        }
+        """.toString()
+
+    commandRequestBodyList << """
+        {
+          "userIdentifier": "standard-customer@cargotracker.com",
+          "routeSpecification": {
+            "originLocation": "DEHAM",
+            "destinationLocation": "NLRTM",
+            "departureEarliestTime": "${ departureEarliestTime }",
+            "departureLatestTime": "${ departureLatestTime }",
+            "arrivalLatestTime": "${ arrivalLatestTime }"
+          },
+          "commodityInfo": {
+            "commodityType": "dry",
+            "totalWeightInKilograms": 45000
+          },
+          "containerDimensionType": "DIMENSION_ISO_22"
+        }
+        """.toString()
+
+    return commandRequestBodyList
+  }
+
   static String makeQueryRequestBody_bookingOfferSummary_findById(String bookingOfferIdentifier) {
     String queryRequestBody = """
       {
         "userIdentifier": "standard-customer@cargotracker.com",
         "bookingOfferIdentifier": "${ bookingOfferIdentifier }"
+      }
+      """
+
+    return queryRequestBody
+  }
+
+  static String makeQueryRequestBody_bookingOfferSummary_findAll_standardCustomer() {
+    String queryRequestBody = """
+      {
+        "userIdentifier": "standard-customer@cargotracker.com"
+      }
+      """
+
+    return queryRequestBody
+  }
+
+  static String makeQueryRequestBody_bookingOfferSummary_searchAll() {
+    String queryRequestBody = """
+      {
+        "userIdentifier": "standard-customer@cargotracker.com",
+        "customerTypeSearchList": [
+          "STANDARD",
+          "GOLD"
+        ],
+        "originLocationName": "Rijeka",
+        "destinationLocationCountryName": "The United States",
+        "commodityTotalWeightKgFromIncluding": 15000,
+        "commodityTotalWeightKgToIncluding": 100000
       }
       """
 
