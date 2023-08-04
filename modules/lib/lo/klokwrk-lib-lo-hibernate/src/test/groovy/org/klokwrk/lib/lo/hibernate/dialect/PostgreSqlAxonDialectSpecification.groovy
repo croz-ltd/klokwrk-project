571886a8c559fde32ccb7b3845fbbc49209d97fe
Damir Murat <damir.murat.git@gmail.com>
2023-08-04 17:37:14 +0200

    Repackaging in klokwrk-lib-lo-hibernate
    org.klokwrk.lib.hibernate.dialect -> org.klokwrk.lib.lo.hibernate.dialect

--- modules/lib/lo/klokwrk-lib-lo-hibernate/src/test/groovy/org/klokwrk/lib/lo/hibernate/dialect/PostgreSqlAxonDialectSpecification.groovy
+++ modules/lib/lo/klokwrk-lib-lo-hibernate/src/test/groovy/org/klokwrk/lib/lo/hibernate/dialect/PostgreSqlAxonDialectSpecification.groovy
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.klokwrk.lib.hibernate.dialect
+package org.klokwrk.lib.lo.hibernate.dialect
 
 import spock.lang.Specification
 
