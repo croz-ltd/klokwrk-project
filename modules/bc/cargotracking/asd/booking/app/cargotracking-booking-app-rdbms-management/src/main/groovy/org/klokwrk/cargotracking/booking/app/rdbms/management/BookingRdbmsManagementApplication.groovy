6ff74f74ac0ba63e510ad4d1f408cdb02e5eed77
Damir Murat <damir.murat.git@gmail.com>
2023-08-05 14:29:47 +0200

    Repackaging in cargotracking-booking-app-rdbms-management module
    org.klokwrk.cargotracker.booking.rdbms.management -> org.klokwrk.cargotracking.booking.app.rdbms.management

--- modules/bc/cargotracking/asd/booking/app/cargotracking-booking-app-rdbms-management/src/main/groovy/org/klokwrk/cargotracking/booking/app/rdbms/management/BookingRdbmsManagementApplication.groovy
+++ modules/bc/cargotracking/asd/booking/app/cargotracking-booking-app-rdbms-management/src/main/groovy/org/klokwrk/cargotracking/booking/app/rdbms/management/BookingRdbmsManagementApplication.groovy
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.klokwrk.cargotracker.booking.rdbms.management
+package org.klokwrk.cargotracking.booking.app.rdbms.management
 
 import groovy.transform.CompileStatic
 import org.springframework.boot.SpringApplication
